eb603d4e8c2bcc9edbadedbd611220937b210e5f
Carlos E <elizaldc@protonmail.com>
2021-12-07 11:39:25 -0600

    Added singleRound() function and game() function with basic logic operators.

--- script.js
+++ script.js
@@ -1,7 +1,52 @@
+//Function allows the computer to make a move.
 function computerPlay() {
     let array = ['Rock', 'Paper', 'Scissors'];
     let randomMove = array[Math.floor(Math.random() * array.length)];
     return randomMove;
 }
 
-console.log(computerPlay());
+//Function determines the conditions of a single round and the result of a round.
+function singleRound(playerSelection, computerSelection) {
+    let result = '';
+    playerSelection = playerSelection.toUpperCase();
+    
+    if ((playerSelection == "ROCK") && (computerSelection == 'Scissors')) {
+        return result = `You win! your ${playerSelection} beats ${computerSelection}`;
+    }
+    else if ((playerSelection == "SCISSORS") && (computerSelection == "Paper")){
+        return result = `You win! your ${playerSelection} beats ${computerSelection}`;
+    }
+    else if ((playerSelection == 'PAPER') && (computerSelection == 'Rock')){
+        return result = `You win! your ${playerSelection} beats ${computerSelection}`;
+    }
+    else if ((computerSelection == 'Rock') && (playerSelection == "SCISSORS")) {
+        return result = `You lose. ${computerSelection} beats your ${playerSelection}`;
+    }
+    else if ((computerSelection == "Scissors") && (playerSelection == "PAPER")) {
+        return result = `You lose. ${computerSelection} beats your ${playerSelection}`;
+    }
+    else if ((computerSelection == "Paper") && (playerSelection == "ROCK")) {
+        return result = `You lose. ${computerSelection} beats your ${playerSelection}`;
+    }
+    else {
+        return result = "Bummer, it was a tie.";
+    }  
+}
+
+//Basic function just calls the round function and executes it 5 times. Will fix it with a 
+//loop later.
+function game() {
+    playerSelection = prompt("Enter Rock, Paper, or Scissors")
+    console.log(singleRound(playerSelection, computerPlay()));
+    playerSelection = prompt("Enter Rock, Paper, or Scissors")
+    console.log(singleRound(playerSelection, computerPlay()));
+    playerSelection = prompt("Enter Rock, Paper, or Scissors")
+    console.log(singleRound(playerSelection, computerPlay()));
+    playerSelection = prompt("Enter Rock, Paper, or Scissors")
+    console.log(singleRound(playerSelection, computerPlay()));
+    playerSelection = prompt("Enter Rock, Paper, or Scissors")
+    console.log(singleRound(playerSelection, computerPlay()));
+}
+
+//Calls the game
+game();
