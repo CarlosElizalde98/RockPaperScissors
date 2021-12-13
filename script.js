a07e690c461c080071c422cd8328b96fcab9a63c
Carlos E <elizaldc@protonmail.com>
2021-12-13 14:22:19 -0600

    Overhauled the code and several methods to keep score and gameplay seperate.

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+let playerScore = 0;
+let compScore = 0;
+const buttons = document.querySelectorAll('button');
+const results = document.querySelector('#results');
+const content =document.createElement('div');
+
+buttons.addEventListener('click', playGame());
+
 //Function allows the computer to make a move.
 function computerPlay() {
     let array = ['Rock', 'Paper', 'Scissors'];
@@ -8,32 +16,22 @@ function computerPlay() {
 //Function determines the conditions of a single round and the result of a round.
 function playSingleRound(playerSelection, computerSelection) {
     let result = '';
-    let playerScore = 0;
-    let computerScore = 0;
     playerSelection = playerSelection.toUpperCase();
     
-        if ((playerSelection == "ROCK") && (computerSelection == 'Scissors')) {
-            playerScore += 1;
+        if (((playerSelection == "ROCK") && (computerSelection == 'Scissors')) || 
+        
+        ((playerSelection == "SCISSORS") && (computerSelection == "Paper")) ||
+        
+        ((playerSelection == 'PAPER') && (computerSelection == 'Rock'))) {
+          
             return result = `You win! your ${playerSelection} beats ${computerSelection}`;
         }
-        else if ((playerSelection == "SCISSORS") && (computerSelection == "Paper")){
-            playerScore += 1;
-            return result = `You win! your ${playerSelection} beats ${computerSelection}`;
-        }
-        else if ((playerSelection == 'PAPER') && (computerSelection == 'Rock')){
-            playerScore += 1;
-            return result = `You win! your ${playerSelection} beats ${computerSelection}`;
-        }
-        else if ((computerSelection == 'Rock') && (playerSelection == "SCISSORS")) {
-            computerScore += 1;
-            return result = `You lose. ${computerSelection} beats your ${playerSelection}`;
-        }
-        else if ((computerSelection == "Scissors") && (playerSelection == "PAPER")) {
-            computerScore += 1;
-            return result = `You lose. ${computerSelection} beats your ${playerSelection}`;
-        }
-        else if ((computerSelection == "Paper") && (playerSelection == "ROCK")) {
-            computerScore += 1;
+        else if (((computerSelection == 'Rock') && (playerSelection == "SCISSORS")) || 
+        
+        ((computerSelection == "Scissors") && (playerSelection == "PAPER")) || 
+
+        ((computerSelection == "Paper") && (playerSelection == "ROCK"))) {
+
             return result = `You lose. ${computerSelection} beats your ${playerSelection}`;
         }
         else {
@@ -42,35 +40,45 @@ function playSingleRound(playerSelection, computerSelection) {
     
 }
 
-function scoreKeeper(scoreString, playerScore, compScore) {
-    if (scoreString.includes("win")) {
-        playerScore += 1;
+//Function keeps track of the score and updates accordingly.
+function updateGameScore(choice) {
+    let gameResult;
+    gameResult = playSingleRound(choice, computerPlay());
+    content.textContent=gameResult;
+    results.appendChild(content);
+
+
+    if (gameResult.includes("win") && playerScore < 5){
+        playerScore++;
+        return "Player score: " + playerScore + '  Computer Score: ' + compScore;
     }
-    else if (scoreString.includes("lose")){
-        compScore += 1;
+    else if (gameResult.includes("lose") && compScore < 5){
+        compScore++;
+        return "Player Score: " + playerScore + "  Computer Score: " + compScore;
     }
-    else {
+    else if (gameResult.includes('tie')) {
         return "Tie";
     }
-    return [playerScore, compScore];
+    else {
+
+    }
+
 }
 
-
-
 //Button Event Listener
-const buttons = document.querySelectorAll('button');
-const results = document.querySelector('#results');
-const content = document.createElement('div');
-const score = document.createElement('p');
+//Plays the game and attaches results to screen.
+function playGame() {
+    const buttons = document.querySelectorAll('button');
+    const results = document.querySelector('#results');
+    const content = document.createElement('div');
+    const score = document.createElement('p');
 
-let playerScore = 0;
-let compScore = 0;
-let scoreNum = '';
-buttons.forEach((button) => {
-    button.addEventListener('click', () => {
-        let result = playSingleRound(button.id, computerPlay()); //Every time I click a button, a round is played.
-        content.textContent=result;
-        results.appendChild(content);
+    buttons.forEach((button) => {
+        button.addEventListener('click', () => {
+            let result = updateGameScore(button.id); //Every time I click a button, a round is played.
+            content.textContent=result;
+            results.appendChild(content);
 
-    })
-});
+        })
+    });
+}
